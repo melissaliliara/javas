1e0c2026fe2077a3bdc8a85d923a9fdb9ae97c76
Mel <melissaliliara@yahoo.com.br>
2025-03-04 11:59:33 -0300

    teste

---
+++ TestaVariaveis.java
@@ -0,0 +1,12 @@
+public class TestaVariaveis {
+
+public static void main (string[]args){
+    System.out.println ("Olá, novo teste!");
+
+int idade;
+idade= 37;
+
+System.out.println(idade);
+System.out.printl ("A idade é "+idade);
+}
+}
